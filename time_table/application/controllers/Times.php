27ddb4ffeccadbe137f36f6005b8caaeeeac35a6
Janatbek <janatbek@gmail.com>
2016-07-18 16:25:05 -0700

    finished Time- display project

---
+++ time_table/application/controllers/Times.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Times extends CI_Controller {
+	
+
+	public function main()
+	{
+		$this->load->view('time_view');
+	}
+
+}
